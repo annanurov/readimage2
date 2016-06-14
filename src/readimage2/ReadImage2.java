8442fef39e5c12d13bdbe6bb4ad0c515cad3fcbc
Bayram <Bayram@192.168.0.114>
2016-06-14 17:24:23 +0700

    Merge origin/master
    Conflicts:
    	src/readimage/ReadImage.java
    	src/readimage/sampleSet.java

---
+++ src/readimage2/ReadImage2.java
@@ -0,0 +1,17 @@
+
+package readimage2;
+
+/**
+ *
+ * @author Bayram
+ */
+public class ReadImage2 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        sampleDirectory bukvy = new sampleDirectory("samples\\s1a");
+        bukvy.FT2D();
+
+
+    }
+    
+}
