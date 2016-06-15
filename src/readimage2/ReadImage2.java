08001f00a53bdcc4b282f9ea882eae46297798ec
Bayram <Bayram@192.168.0.114>
2016-06-15 14:26:07 +0700

    Merge origin/master
    Conflicts:
    	src/readimage/ReadImage.java
    	src/readimage/sampleSet.java

--- src/readimage2/ReadImage2.java
+++ src/readimage2/ReadImage2.java
@@ -8,9 +8,22 @@ package readimage2;
 public class ReadImage2 {
     public static void main(String[] args) {
         // TODO code application logic here
-        sampleDirectory bukvy = new sampleDirectory("samples\\s1a");
-        bukvy.FT2D();
+        sampleDirectory bukvy = new sampleDirectory("s1a");
+//        bukvy.FT2D();
 
+        String[] dirs = new String []{
+//            "s1b",
+//            "s2a","s2b","s3a","s3b",
+//            "s4a","s4b",
+            "s5a","s5b"
+//                ,
+//            "s6a","s6b","s7a","s7b",
+//            "s8a","s8b","s9a","s9b"
+        };   
+        for(String d : dirs){
+            bukvy = new sampleDirectory(d);
+            bukvy.FT2D();
+        }
 
     }
     
